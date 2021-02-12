8e01c180064072e6373696071107d48db0233721
Maks Makuta <50670334+lakeFm@users.noreply.github.com>
2021-02-12 22:01:59 +0200

    clear project

---
+++ app/src/main/java/ua/mstudio/droid/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package ua.mstudio.droid.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ua.mstudio.droid.R
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/ua/mstudio/droid/models/DroidVersion.kt
@@ -0,0 +1,3 @@
+package ua.mstudio.droid.models
+
+data class DroidVersion(val API : Int, val CodeName : String,val CodeVersion : String)
