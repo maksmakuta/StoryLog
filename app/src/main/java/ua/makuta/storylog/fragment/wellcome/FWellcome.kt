a1fefcb0038fa28d8969183e8549a14a4fdc75e6
Maks Makuta <maksmakuta227@gmail.com>
2022-08-02 10:32:39 +0300

    [1.0 beta 3] add some info and update UI

--- app/src/main/java/ua/makuta/storylog/fragment/wellcome/FWellcome.kt
+++ app/src/main/java/ua/makuta/storylog/fragment/wellcome/FWellcome.kt
@@ -1,7 +1,24 @@
 package ua.makuta.storylog.fragment.wellcome
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ua.makuta.storylog.R
 import ua.makuta.storylog.core.CoreFragment
 
 class FWellcome : CoreFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_wellcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
---
+++ app/src/main/java/ua/makuta/storylog/model/Model.kt
@@ -0,0 +1,9 @@
+package ua.makuta.storylog.model
+
+data class Model(
+    val api : Int,
+    val version: String,
+    val date : String,
+    val logs : String,
+    val codename : String
+)
