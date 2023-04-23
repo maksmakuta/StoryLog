dc5b5ee57cc042e4f31982e5a378468765dee47c
maksmakuta <makuta.maks.mmc@gmail.com>
2023-04-23 22:14:07 +0300

    [1.3-beta 16] add internet checker | add caching for files and images

--- app/src/main/java/ua/makuta/storylog/StoryApp.kt
+++ app/src/main/java/ua/makuta/storylog/StoryApp.kt
@@ -1,20 +1,39 @@
 package ua.makuta.storylog
 
 import android.app.Application
+import android.content.Context
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.decode.SvgDecoder
+import coil.disk.DiskCache
+import okhttp3.Cache
+import java.io.File
 
 class StoryApp : Application(), ImageLoaderFactory {
 
+    companion object{
+        private lateinit var appContext : Context
+        fun cache() = Cache(
+            directory = File(appContext.cacheDir, "http_cache"),
+            maxSize = 64L * 1024L * 1024L // 64 MiB
+        )
+    }
+
     override fun onCreate() {
         super.onCreate()
+        appContext = this.applicationContext
     }
 
     override fun newImageLoader() = ImageLoader.Builder(this)
-            .components {
-                add(SvgDecoder.Factory())
-            }
-            .build()
+        .diskCache(
+            DiskCache.Builder()
+                .directory(File(appContext.cacheDir, "coil_cache"))
+                .maxSizeBytes(128L * 1024L * 1024L)
+                .build()
+        )
+        .components {
+            add(SvgDecoder.Factory())
+        }
+        .build()
 
 }
--- app/src/main/java/ua/makuta/storylog/activity/MainViewModel.kt
+++ app/src/main/java/ua/makuta/storylog/activity/MainViewModel.kt
@@ -1,19 +1,51 @@
 package ua.makuta.storylog.activity
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import ua.makuta.storylog.model.ModelMenu
 import ua.makuta.storylog.model.ModelVersion
+import ua.makuta.storylog.net.BASE_URL
 import ua.makuta.storylog.net.storyNet
 import ua.makuta.storylog.utils.Utils.logger
+import java.io.IOException
 
 class MainViewModel : ViewModel() {
 
     val data = MutableLiveData<ModelMenu?>(null)
     val arrData = MutableLiveData<ArrayList<ModelVersion>?>(null)
     val item = MutableLiveData<ModelVersion?>(null)
+    val hasInternet = MutableLiveData<Boolean?>(null)
+
+    fun checkNetwork(){
+        val r = Request.Builder()
+            .get()
+            .url(BASE_URL + "settings.gradle")
+            .build()
+        OkHttpClient().newCall(r).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                hasInternet.postValue(false)
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                hasInternet.postValue(response.code == 200)
+            }
+
+        })
+    }
 
     fun loadMenu(){
         viewModelScope.launch {
